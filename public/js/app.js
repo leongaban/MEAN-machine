ea0a068134eba692ec6fbf21a2054ce5ea5a58ab
Leon Gaban <lg209c@att.com>
2015-02-19 10:40:39 -0600

    added animations to ng-route

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 /*global angular*/
 
 // name our angular app
-var app = angular.module('routerApp', ['ngRoute'])
+var app = angular.module('routerApp', ['ngRoute', 'routerRoutes', 'ngAnimate'])
 
 // controller for ENTIRE site
 .controller('mainController', function() {
@@ -33,40 +33,4 @@ var app = angular.module('routerApp', ['ngRoute'])
 	
 	var vm = this;
 	vm.message = 'Contact us! JK. This is just a demo.';
-})
-
-
-// inject ngRoute for all our routing needs
-// angular.module('routerRoutes', ['ngRoute'])
-// configure our routes
-.config(function($routeProvider, $locationProvider) {
-	$routeProvider
-	
-		// route for the home page
-	    .when('/', {
-	        templateUrl : 'views/pages/home.html',
-	        controller  : 'homeController',
-	        controllerAs: 'home'
-		})
-		// route for the about page
-		.when('/about', {
-			templateUrl : 'views/pages/about.html',
-			controller  : 'aboutController',
-			controllerAs: 'about'
-		})
-		// route for the contact page
-		.when('/contact', {
-			templateUrl : 'views/pages/contact.html',
-			controller  : 'contactController',
-			controllerAs: 'contact'
-		});
-
-	// set our app up to have pretty URLS
-	$locationProvider.html5Mode(true);
 });
-
-
-
-
-
-
